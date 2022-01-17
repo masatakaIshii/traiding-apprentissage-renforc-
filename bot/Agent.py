a01da8fd71d4b033a6050b3c2f76df06583914a4
JeremTer <ternisien09@gmail.com>
2022-01-17 19:01:58 +0100

    Start to implement agent

--- bot/Agent.py
+++ bot/Agent.py
@@ -5,6 +5,9 @@
 # - learning rate
 # - update avec les rewards
 # - les rewards du coup lol
+from bot.Action import Action
+from bot.State import State
+
 REWARD_VERY_HIGH = 1000
 REWARD_HIGH = 500
 REWARD_LITTLE_HIGH = 200
@@ -12,37 +15,38 @@ REWARD_LITTLE_LOW = -200
 REWARD_LOW = -500
 REWARD_VERY_LOW = -1000
 
-ACTIONS = [BUY, KEEP, SELL]
-
-
-# Quel sera notre environnement finalement ?
-#
 class Agent:
     def __init__(self, environment, learning_rate=1, discount_factor=0.5):
-        # environnement -> notre partie logique je pense ?
-        # il nous faudra au moins le portefeuille
+        self.__states = [State.VERY_LOW, State.LOW, State.LITTLE_LOW, State.LITTLE_HIGH, State.HIGH, State.VERY_HIGH]
+        self.__actions = [Action.BUY, Action.KEEP, Action.SELL]
         self.__learning_rate = learning_rate
         self.__discount_factor = discount_factor
         self.__environment = environment
         self.__qtable = {}
-        # TODO voir comment implémenter la qtable
-        # for s in self.__environment.states:
-        #     self.__qtable[s] = {}
-        #     for a in ACTIONS:
-        #         self.__qtable[s][a] = 0.0
+
+        for s in self.__states:
+            self.__qtable[s] = {}
+            for a in self.__actions:
+                self.__qtable[s][a] = 0.0
         self.reset()
 
-    # TODO IMPLEMENT
-    # Update sert à mettre à jour l'état, le score et la qtable
-    def update(self, state, action, reward):
-        return
-
-    # TODO IMPLEMENT
-    # Best action sert à trouver la meilleure action à faire
-    # En fonction de notre état et de notre Qtable
-    def best_action(self):
-        return
-
     def reset(self):
-        self.__state = self.__environment.start
+        self.__state = State.LITTLE_LOW
         self.__score = 0
+
+    def update(self, state, action, reward):
+        maxQ = max(self.__qtable[state].values())
+        self.__qtable[self.__state][action] += self.__learning_rate * \
+                                               (reward + self.__discount_factor * maxQ - self.__qtable[self.__state][
+                                                   action])
+
+        self.__state = state
+        self.__score += reward
+
+    def best_action(self):
+        best = None
+        for a in self.__qtable[self.__state]:
+            if not best \
+                    or self.__qtable[self.__state][a] > self.__qtable[self.__state][best]:
+                best = a
+        return best
---
+++ bot/State.py
@@ -0,0 +1,10 @@
+from enum import Enum
+
+
+class State(Enum):
+    VERY_LOW = 0
+    LOW = 1
+    LITTLE_LOW = 2
+    LITTLE_HIGH = 3
+    HIGH = 4
+    VERY_HIGH = 5
