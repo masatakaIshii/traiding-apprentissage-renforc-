17eafbe437b3214d748efbb9570d65775a84d793
MasatakaIshii <masaradio78@gmail.com>
2022-01-30 17:08:14 +0100

    form progress get end or start date done

--- gui/StockFormController.py
+++ gui/StockFormController.py
@@ -1,14 +1,45 @@
+from datetime import datetime, timedelta
+
 from gui import StockFormView
 
 
 class StockFormController:
-    def __init__(self, view: StockFormView):
+    def __init__(self, view: StockFormView, interval: int = 14):
         self.view = view
         self.selected_date = ""
         self.view.validate_button.bind("<Button>", self.validate_date)
+        self.interval = interval
+
+        self.__is_start_date = True
+        self.view.get_start_date_button.bind("<Button>", self.open_calendar_for_start_date)
+        self.view.get_end_date_button.bind("<Button>", self.open_calendar_for_end_date)
+
+        self.view.calender.bind("<<CalendarSelected>>", self.__date_selected)
+
+    def open_calendar_for_start_date(self, _):
+        print("open calender for start date")
+        self.__is_start_date = True
+        if self.view.end_date_value['text'] != 'Not yet':
+            start_date = datetime.strptime(self.view.end_date_value['text'], '%Y-%m-%d')
+            self.view.calender['maxdate'] = start_date - timedelta(days=1 + self.interval)
+        self.view.show_calender()
+
+    def open_calendar_for_end_date(self, _):
+        self.__is_start_date = False
+        if self.view.start_date_value['text'] != 'Not yet':
+            start_date = datetime.strptime(self.view.start_date_value['text'], '%Y-%m-%d')
+            self.view.calender['mindate'] = start_date + timedelta(days=1 + self.interval)
+        self.view.show_calender()
 
     def validate_date(self, _):
         self.selected_date = self.view.calender.get_date()
         print(f"selection date : {self.selected_date}")
 
-
+    def __date_selected(self, _):
+        print(f"date selected : {self.view.calender.get_date()}")
+        new_date = self.view.calender.get_date()
+        if self.__is_start_date:
+            self.view.update_start_date(new_date)
+        else:
+            self.view.update_end_date(new_date)
+        self.view.hide_calender()
--- gui/StockFormView.py
+++ gui/StockFormView.py
@@ -7,19 +7,59 @@ class StockFormView(tkinter.Frame):
     def __init__(self, master=None):
         super().__init__(master=master)
 
-        self.calender_container = tkinter.LabelFrame(self, text="Fetch new stock")
+        fetch_stock_container = tkinter.LabelFrame(self, text="Fetch new stock")
 
-        self.validate_button = tkinter.Button(self.calender_container, text="Validate")
-        self.validate_button.grid(row=0, pady=10)
+        # Stock form
+        stock_form_container = tkinter.LabelFrame(fetch_stock_container)
 
-        calender_title = tkinter.Label(self.calender_container, text="Calender title")
-        calender_title.grid(row=0, column=1)
+        stock_name_label = tkinter.Label(stock_form_container, text="Stock name :")
+        stock_name_label.grid(row=0, column=0)
+        self.stock_name_value = tkinter.Entry(stock_form_container)
+        self.stock_name_value.grid(row=0, column=1)
+
+        start_date_label = tkinter.Label(stock_form_container, text="Start date :")
+        start_date_label.grid(row=1, column=0)
+        self.start_date_value = tkinter.Label(stock_form_container, text="Not yet")
+        self.start_date_value.grid(row=1, column=1)
+        self.get_start_date_button = tkinter.Button(stock_form_container, text="Get date")
+        self.get_start_date_button.grid(row=1, column=2)
+
+        end_date_label = tkinter.Label(stock_form_container, text="End date :")
+        end_date_label.grid(row=2, column=0)
+        self.end_date_value = tkinter.Label(stock_form_container, text="Not yet")
+        self.end_date_value.grid(row=2, column=1)
+        self.get_end_date_button = tkinter.Button(stock_form_container, text="Get date")
+        self.get_end_date_button.grid(row=2, column=2)
+
+        self.validate_button = tkinter.Button(stock_form_container, text="Validate")
+        self.validate_button.grid(row=3, columnspan=3, pady=10)
+
+        stock_form_container.grid(row=0, column=0)
+
+        self.calender_container = tkinter.LabelFrame(fetch_stock_container)
+
+        self.calender_title = tkinter.Label(self.calender_container, text="Calender title")
+        self.calender_title.grid(row=0, column=1)
+
         self.calender = Calendar(self.calender_container, selectmode="day", year=2020, month=5, day=22,
                                  date_pattern='y-mm-dd')
         self.calender.grid(row=1, column=1, pady=5, padx=20)
+        # self.calender_container.grid(row=0, column=1)
 
-        self.calender_container.pack()
+        fetch_stock_container.pack()
 
         self.grid(row=0, column=1, columnspan=3, padx=10, pady=5)
 
         # self.calender_container.grid_remove()
+
+    def show_calender(self):
+        self.calender_container.grid(row=0, column=1)
+
+    def hide_calender(self):
+        self.calender_container.grid_remove()
+
+    def update_start_date(self, start_date: str):
+        self.start_date_value['text'] = start_date
+
+    def update_end_date(self, end_date: str):
+        self.end_date_value['text'] = end_date
